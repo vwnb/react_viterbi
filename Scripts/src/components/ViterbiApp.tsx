b0f3fdc7af1abfd15218a332d3942247b36ba5be
Ville Kemppainen <villekemppainen@Villes-MacBook-Pro.local>
2023-02-02 08:00:02 +0200

    Add rambling

--- Scripts/src/components/ViterbiApp.tsx
+++ Scripts/src/components/ViterbiApp.tsx
@@ -21,8 +21,12 @@ function ViterbiApp(props) {
             <p key={state.key}>While {state.observation}ing, it was {state.name === 'sunny' ? state.name + ". 🌞" : state.name + ". 🌧️"}</p>
         ))}
         <TextChurner emissionProbability={state.input.emission_probability} />
-        <p>ℹ️ This is a demonstration of "hidden states" revealed by a Javascript implementation of the <a href="https://en.wikipedia.org/wiki/Viterbi_algorithm">Viterbi algorithm.</a></p>
-      </div>
+        <h3>What?</h3>
+        <p>This is a demonstration of "hidden states" revealed by a Javascript implementation of the <a href="https://en.wikipedia.org/wiki/Viterbi_algorithm">Viterbi algorithm.</a></p>
+        <h3>Insights</h3>
+        <p>Setting the emission probabilities to extremes like 0 and 1 has a less extreme effect than setting them around 0.4 to 0.6. This is useful when modeling a very sunny or very rainy weather.</p>
+        <p>The <a href="https://en.wikipedia.org/wiki/Hidden_Markov_model">Hidden Markov Models</a> act in mysterious ways but the function should be pure! Same input, same output.</p>
+     </div>
       <Controls input={state.input} />
     </div>
   );
