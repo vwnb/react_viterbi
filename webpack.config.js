ea153fe0a53da21ed9628b2f75084d16d2db8da8
Ville Kemppainen <villekemppainen@Villes-MacBook-Pro.local>
2023-02-10 09:01:06 +0200

    Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+
 module.exports = {
-    entry: "./Scripts/src/index.tsx",
+    entry: {
+      app: "./Scripts/src/index.tsx"
+    },
+
     output: {
       filename: "bundle.js",
     },
   
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
+
+    devServer: {
+      static: {
+        directory: path.join(__dirname, '/')
+      },
+      hot: true,
+    },
   
     resolve: {
       // Add '.ts' and '.tsx' as resolvable extensions.
       extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Hot Module Replacement',
+        template: 'index.html',
+        filename: 'index.html',
+        inject: 'body'
+      })
+    ],
   
     module: {
       rules: [
